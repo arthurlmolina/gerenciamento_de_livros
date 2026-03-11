6ba6c580d6c7530576ae5095e4d005ece993a6fc
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-11 20:10:57 -0300

    repositorio livro criado

---
+++ src/main/java/com/example/gerenciamento_de_livros/repositories/LivroRepository.java
@@ -0,0 +1,7 @@
+package com.example.gerenciamento_de_livros.repositories;
+
+import com.example.gerenciamento_de_livros.models.LivroModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroRepository extends JpaRepository<LivroModel, Long> {
+}
